6da1c3d315a29dbf4499091540b88ffdf51200a6
Kripak Igor <ipkforwork@gmail.com>
2022-12-17 12:33:27 +0500

    init

---
+++ src/main/java/com/example/seven_winds/SevenWindsApplication.java
@@ -0,0 +1,13 @@
+package com.example.seven_winds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenWindsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenWindsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seven_winds/SevenWindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seven_winds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenWindsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
