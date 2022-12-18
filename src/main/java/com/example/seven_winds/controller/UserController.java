3551cf5286bffc685c1323394934e715d0566b38
Kripak Igor <ipkforwork@gmail.com>
2022-12-18 23:40:45 +0500

    add exception handlers to "getUserByID", "deleteUser" in UserController

--- src/main/java/com/example/seven_winds/controller/UserController.java
+++ src/main/java/com/example/seven_winds/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.example.seven_winds.controller;
 
-
 import com.example.seven_winds.model.User;
 import com.example.seven_winds.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.server.ResponseStatusException;
+import org.webjars.NotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -28,7 +29,11 @@ public class UserController {
     @GetMapping("/{id}")
     @Operation(summary = "Getting user by ID from DB")
     public User getUserById(@PathVariable long id) {
-        return userService.getUserByID(id);
+        try {
+            return userService.getUserByID(id);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 
     @PostMapping
@@ -40,6 +45,10 @@ public class UserController {
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete users by ID from DB")
     public void deleteUser(@PathVariable long id) {
-        userService.deleteUser(id);
+        try {
+            userService.deleteUser(id);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
